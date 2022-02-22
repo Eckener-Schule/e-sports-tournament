c3f5fbcdae9012ae4945d1958ef183da9198d354
Mleiss90 <99392542+Mleiss90@users.noreply.github.com>
2022-02-22 08:46:57 +0100

    Update config.php

--- login/config.php
+++ login/config.php
@@ -1,11 +1,10 @@
 <?php
-/*  */
+/* define your own spezification for your database  */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'userdetails');
  
-/*  */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
